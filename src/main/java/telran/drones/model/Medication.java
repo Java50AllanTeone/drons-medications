9a1335479c9332f5ac419c50913de6e72ae78dfd
allanteone <allanteone@gmail.com>
2024-01-28 18:04:41 +0200

    Medication entity fixes

--- src/main/java/telran/drones/model/Medication.java
+++ src/main/java/telran/drones/model/Medication.java
@@ -1,6 +1,27 @@
 package telran.drones.model;
 
-//TODO create entity Medication
+import jakarta.persistence.*;
+import lombok.*;
+import telran.drones.dto.*;
+
+@Entity
+@Table(name="medications")
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Medication {
+	
+	String name;
+	Integer weight;
+	@Id
+	@Column(name="medication_code")
+	String code;
+
+	static public Medication of(DroneMedication droneMedication) {
+		return new Medication(null, null, droneMedication.medicationCode());
+	}
+	
+	public DroneMedication build() {
+		return new DroneMedication(null, code);
+	}
 
 }
