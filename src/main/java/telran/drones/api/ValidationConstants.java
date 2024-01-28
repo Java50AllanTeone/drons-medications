4804068c52a5a53f24778e8f5c7fd4fec1057f36
allanteone <allanteone@gmail.com>
2024-01-28 22:29:18 +0200

    DTO fixes

--- src/main/java/telran/drones/api/ValidationConstants.java
+++ src/main/java/telran/drones/api/ValidationConstants.java
@@ -4,9 +4,10 @@ public interface ValidationConstants {
 	int MAX_DRONE_NUMBER_LENGTH = 100;
 	String MEDICATION_REGEXP = "^[A-Z0-9_]*$";
 	String MISSING_DRONE_NUMBER_MESSAGE = "Missing drone number";
-	String MISSING_MEDICATION_CODE = "Missing medication code";
-	String WRONG_DRONE_NUMBER = "Drone number must be less or equal " + MAX_DRONE_NUMBER_LENGTH;
-	String WRONG_MEDICATION_CODE = "Medication code can contains upper case letters, underscore, numbers";
+	String MISSING_MEDICATION_CODE_MESSAGE = "Missing medication code";
+	String MISSING_MODEL_TYPE_MESSAGE = "Missing model type";
+	String WRONG_DRONE_NUMBER_MESSAGE = "Drone number must be less or equal " + MAX_DRONE_NUMBER_LENGTH;
+	String WRONG_MEDICATION_CODE_MESSAGE = "Medication code can contains upper case letters, underscore, numbers";
 	
 	
 
--- src/main/java/telran/drones/dto/DroneDto.java
+++ src/main/java/telran/drones/dto/DroneDto.java
@@ -1,6 +1,14 @@
 package telran.drones.dto;
-//TODO add validation constraints
-public record DroneDto(String number, ModelType modelType) {
+
+
+import static telran.drones.api.ValidationConstants.*;
+import jakarta.validation.constraints.*;
+
+public record DroneDto(
+		@NotEmpty(message = MISSING_DRONE_NUMBER_MESSAGE) 
+		@Size(max = 100, message=WRONG_DRONE_NUMBER_MESSAGE)
+		String number, 
+		ModelType modelType) {
 	
 
 }
